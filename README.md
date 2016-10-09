b10b9dcf02d03ae3851c6c9926fce92ce7db98f7
teresalazar13 <teresa.sal13@gmail.com>
2016-10-09 18:39:32 +0100

    hello world

---
+++ README.md
@@ -0,0 +1,5 @@
+# door-slack-bot
+
+This is a simple slack bot we use to choose which person is opening the door to other people.
+
+We have our team members based on departments listed at `members/<department>.json`.
---
+++ app.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+var app = express();
+var port = process.env.PORT || 1337;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res) {
+    console.log('GET /');
+
+    res.status(200).send('Door');
+});
+
+var pickRandom = function(department) {
+    var members = JSON.parse(fs.readFileSync('members/' + department + '.json', 'utf8'));
+    return members[Math.floor(Math.random() * members.length)];
+};
+
+app.post('/random/:department', function(req, res, next) {
+    console.log("POST /random");
+    var username = pickRandom(req.params.department);
+    var botPayload = {
+        text: '@' + username + ', é a tua vez de abrir a porta :grin:'
+    };
+    return res.status(200).json(botPayload);
+});
+
+app.post('/door', function(req, res, next) {
+    console.log("POST /door");
+
+    var username = req.body.user_name;
+    var botPayload = {
+        text: username + ' pede para abrirem a :door:'
+    };
+
+    return res.status(200).json(botPayload);
+});
+
+app.listen(port, function() {
+    console.log('Listening on port ' + port);
+});
